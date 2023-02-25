5edc10a11b48314b21a7cdf984748f70f61c488a
Essadi <Essadi@DESKTOP-TMGECJG>
2023-02-25 15:11:10 +0100

    fgfg

---
+++ CafeForGames/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using CafeForGames.Repository;
+using Microsoft.AspNetCore.Mvc;
+using CafeForGames.Models;
+
+namespace CafeForGames.Controllers
+{
+    public class HomeController : Controller
+    {
+        IGamerepository<Games> _CafeForGames;
+        public HomeController(IGamerepository<Games> CafeForGames)
+        {
+            _CafeForGames = CafeForGames;
+        }
+        public IActionResult Index()
+        {
+            var result = _CafeForGames.List();
+            return View(result);
+        }
+    }
+}
