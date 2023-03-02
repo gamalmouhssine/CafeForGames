35c89408132da3bee67b279e17ac36991725cb12
Essadi <Essadi@DESKTOP-TMGECJG>
2023-03-02 15:36:34 +0100

    last commit app in 3/2/2023

---
+++ CafeForGames/Data/TvOfChannel.cs
@@ -0,0 +1,10 @@
+﻿namespace CafeForGames.Data
+{
+    public class TvOfChannel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public string Logo { get; set; }
+    }
+}
